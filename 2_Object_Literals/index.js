b17beeb051b8f04db462e9c5f530b6a17576fd7b
John Lyons <jwlyons2015@gmail.com>
2026-01-31 21:54:15 -0500

    2_Object_Literals

---
+++ 2_Object_Literals/index.js
@@ -0,0 +1,21 @@
+// {} is object literal syntax
+const circle = {
+    // key value pairs:
+    radius: 1,
+    location: {
+        x: 1,
+        y: 1
+    },
+    draw: function () {
+        console.log('draw');
+    }
+};
+
+// 
+// circle object has 3 members: radius, location, draw
+// method - fuction inside syntax literal
+// member - things inside {}
+// properties - annything not a function inside {}
+
+
+circle.draw();
