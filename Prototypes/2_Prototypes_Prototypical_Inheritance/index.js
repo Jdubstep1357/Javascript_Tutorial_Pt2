93a97238c53604d69e050d6da81544a6e7aac9e3
John Lyons <jwlyons2015@gmail.com>
2026-02-03 16:34:46 -0500

    2_Prototypes_Prototypical_Inheritance

---
+++ Prototypes/2_Prototypes_Prototypical_Inheritance/index.js
@@ -0,0 +1,20 @@
+// Prototype - parent of other
+
+// define another object - shape and add all common methods for object
+// link circle with shape object
+
+// shape - protoype - parent of other object
+let x = {};
+
+/*  ------------- This shows objectBase -----------
+    x -> (links to parent) objectBase - Prototype
+*/
+
+
+// every object directly or inderectly is from objectBase
+
+// when accessing method a property or method on object javascript mocks up 
+// chain to find parent member
+
+// prototype is just a regular object
+// only root object doesnt have prototype
