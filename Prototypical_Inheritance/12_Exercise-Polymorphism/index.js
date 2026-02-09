fa186b335a8ce1fe35e68a223f2cd3454dd11ced
John Lyons <jwlyons2015@gmail.com>
2026-02-09 10:33:13 -0500

    13_Polymorphism_Completed

---
+++ Prototypical_Inheritance/12_Exercise-Polymorphism/index.js
@@ -0,0 +1,74 @@
+// Design 2 objects: HTMLElement, HTMLSelectElement
+
+
+// one method - click - prototype other method focus
+// console.log clicked that says clicked. e.focu is focused
+
+
+function HTMLElement() {
+    this.click = function () {
+        console.log("Clicked");
+    }
+}
+
+HTMLElement.prototype.focus = function () {
+    console.log("Focused");
+}
+
+
+function HTMLSelectElement(items = []) {
+    this.items = items;
+
+    this.addItem = function (items) {
+        this.items.push(items);
+    }
+
+    // REMOVE ITEM
+    this.removeItem = function (items) {
+        this.items.splice(this.items.indexOf(items), 1);
+    }
+
+    this.render = function () {
+        return `
+        <select>${this.items.map(item => `
+            <option>${item}</option>`).join('')}
+        </select>`
+    }
+
+}
+
+// HTMLSelectElement.prototype.addItem = function () {
+//     items.push();
+//     console.log("Item added");
+// }
+
+// HTMLSelectElement.prototype.removeItem = function () {
+//     console.log("Item removed");
+// }
+
+
+
+const e = new HTMLElement();
+// HTMLSelectElement
+const s = new HTMLSelectElement([]);
+
+
+
+
+/* RENDERING IMAGE */
+
+
+// has items property which is intiially an empty array
+// Has addItem and removeItem
+// s.addItem adds one item and displays it on array
+
+function HtmlImageElement(src) {
+    this.src = src;
+
+    this.render = function () {
+        return `<img src=${this.src}" />`
+    }
+}
+
+HtmlImageElement = prototype = new HTMLElement();
+HtmlImageElement = prototpe.constructor = HtmlImageElement;
