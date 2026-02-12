6f90dff2572a00c83e0f9be4557366697c895d5b
Jdubstep1357 <jwlyons2015@gmail.com>
2026-02-11 19:22:13 -0500

    1_Modules

---
+++ Tooling/index.js
@@ -0,0 +1,14 @@
+// modules are different parts of file with code
+
+// benefits of modules
+// 1. Maintainability
+// 2. Reuse
+// 3. Abstract
+
+/* Modules Formats
+AMD - Asynchronous
+CommonJS - Node.js
+UMD - Browser / Node.js
+ES6 Module
+
+*/
