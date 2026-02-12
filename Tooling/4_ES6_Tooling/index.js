b68ec48cc85efe314eddce3a9a23e35976a73433
Jdubstep1357 <jwlyons2015@gmail.com>
2026-02-11 19:46:22 -0500

    4_ES6_Tooling

---
+++ Tooling/4_ES6_Tooling/index.js
@@ -0,0 +1,4 @@
+// Transpiler - Combination of Translator and combile
+// convert javascript code that old browsers can understand
+
+// Bundler - combines all javascript file into single bundler ie: webpack
