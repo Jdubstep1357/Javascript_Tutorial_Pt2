5ae3f1f9b645cc244235a3e25b02b142ac59b937
Jdubstep1357 <jwlyons2015@gmail.com>
2026-02-11 20:17:12 -0500

    5_Babel

---
+++ Tooling/5_Babel/build/index.js
@@ -0,0 +1,11 @@
+"use strict";
+
+// Transpiler - Combination of Translator and combile
+// convert javascript code that old browsers can understand
+
+// Bundler - combines all javascript file into single bundler ie: webpack
+
+
+// to Install babel FIRST run npm init --yes to get package.json file
+
+var x = 1;
---
+++ Tooling/5_Babel/index.js
@@ -0,0 +1,9 @@
+// Transpiler - Combination of Translator and combile
+// convert javascript code that old browsers can understand
+
+// Bundler - combines all javascript file into single bundler ie: webpack
+
+
+// to Install babel FIRST run npm init --yes to get package.json file
+
+const x = 1;
